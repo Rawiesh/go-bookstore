09f1cfd898f380cd6ad2a180572ddb1600394618
Rawiesh <rawieshbehari@gmail.com>
2023-10-10 18:53:58 -0300

    feat: use env for db connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,14 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,7 +16,17 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	// Fetch db values from env
+	err := godotenv.Load(filepath.Join("../..", ".env"))
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	connectionStr := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_NAME)
+	// Open connection
+	d, err := gorm.Open("mysql", connectionStr)
 	if err != nil {
 		panic(err)
 	}
